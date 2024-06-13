929eff9a8a016f93ed491b47373cb4f9c600380b
lufefe <jumbaayabonga@gmail.com>
2024-06-13 21:16:12 +0200

    Update ContactUs.js

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -52,6 +52,7 @@ const ContactUs = () => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
       },
       body: JSON.stringify({ "g-recaptcha-response": token }),
     })
